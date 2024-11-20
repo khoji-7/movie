9364377b94874afd2cf7735f08a4b59cab06538e
khoji-7 <nematxonovh@gmail.com>
2024-11-20 13:25:34 +0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='text-red-500'>
-      sdalmsd
-     </h1>
-    </div>
+    <main>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </main>
   );
 }
 
