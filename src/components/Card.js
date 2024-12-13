6189b71b0b5334b6dd628ba5ec9b2697baa69b6f
khoji-7 <nematxonovh@gmail.com>
2024-12-13 18:37:27 +0500

    start trending card

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+const Card = ({data}) => {
+
+  return (
+    <div>
+       card
+    </div>
+  )
+}
+
+export default Card
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import React  from 'react'
 import BannerHome from '../components/BannerHome'
+import Card from '../components/Card'
 
 const Home = () => {
+  const trendingData = useSelector(state => state.movieoData.bannerData)
+
   return (
     <div>
       <BannerHome/>
+      
+      {
+        trendingData.map((data)=>{
+          return(
+            <Card key={data.id} data={data}/>
+          )
+        })
+      }
     </div>
   )
 }
